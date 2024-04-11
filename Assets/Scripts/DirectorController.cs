7a3039be30ffa205e0ef80f8f72bdf2313243604
Muna <muna.ongame@gmail.com>
2024-04-11 19:42:25 +0200

    movimiento y pausa arreglado
    animacion de madre para

--- Assets/Scripts/DirectorController.cs
+++ Assets/Scripts/DirectorController.cs
@@ -4,22 +4,27 @@ using UnityEngine.Playables;
 public class DirectorController : MonoBehaviour
 {
     public PlayableDirector timeline;
-    public Animator madre;
-    public float[] tiemposDeDetencion; //Array que indica en que segundos parará la cinematica
+    public Animator animator;
+    public float[] tiemposDeDetencion; // Array que indica en qué segundos parará la cinematica
     public Vector3[] posicionesPausa;
     private int aux = 0; // Índice para recorrer el array
+
+    /*void Start()
+    {
+        animator = GetComponent<Animator>();
+    }*/
 
     void Update()
     {
         if (aux < tiemposDeDetencion.Length && timeline.time >= tiemposDeDetencion[aux])
         {
-            timeline.Pause();
-            madre.transform.position = posicionesPausa[aux];
+            timeline.playableGraph.GetRootPlayable(0).SetSpeed(0f); // Pausa la cinematica
+            animator.Play("Madre");
         }
 
         if (Input.GetButtonDown("Jump"))
         {
-            timeline.Resume();
+            timeline.playableGraph.GetRootPlayable(0).SetSpeed(1f); // Reanuda la cinematica
             aux++;
         }
     }
