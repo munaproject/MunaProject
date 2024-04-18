d7b5da4a50cd01fdb626408932bb7a954befd370
Muna <muna.ongame@gmail.com>
2024-04-18 09:32:07 +0200

    pruebas multijugador

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -9,20 +9,24 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     public TMP_InputField crearField;
     public TMP_InputField unirseField;
+    private bool esMaster;
 
     
     public void crearRoom() {
         //Creamos una 'room'
+        esMaster=true;
         PhotonNetwork.CreateRoom(crearField.text);
     }
 
     public void unirseRoom() {
         //Nos unimos a una 'room'
+        esMaster=false;
         PhotonNetwork.JoinRoom(unirseField.text);
     }
 
     public override void OnJoinedRoom()
     {
+        PlayerPrefs.SetInt("esMaster", true ? 1 : 0);
         PhotonNetwork.LoadLevel("PruebaPhoton");
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,15 +5,22 @@ using Photon.Pun;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject master;
     public GameObject jugador;
 
     public float valueX;
     public float valueY;
+
+    private int esMasterAux;
+    private bool esMaster;
     
     void Start() 
     {
         //Colocamos al jugador
         Vector2 pos = new Vector2(valueX, valueY);
-        PhotonNetwork.Instantiate(jugador.name, pos, Quaternion.identity);
+        esMasterInt = PlayerPrefs.GetInt("esMaster");
+        esMaster = esMasterInt == 1 ? true : false;
+        GameObject jugadorPrefab = esMaster ? master : jugador;
+        PhotonNetwork.Instantiate(jugadorPrefab.name, pos, Quaternion.identity);
     }
 }
