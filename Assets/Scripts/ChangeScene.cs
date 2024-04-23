94318f8d59af3a9fcbcef988d5dab98e826ce358
stacy <muna.ongame@gmail.com>
2024-04-24 00:55:28 +0200

    Cambio entre escenas con ambos jugadores
    Ahora los jugadores se mueven juntos

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -7,13 +7,29 @@ public class ChangeScene : MonoBehaviourPunCallbacks
 {
     public string nextScene;
     private bool activar;
+    PhotonView view;
+
+    void Start() {
+        view = GetComponent<PhotonView>();
+    }
 
     void Update()
     {
         if(Input.GetButtonDown("Jump") && activar)
         {
-            PhotonNetwork.LoadLevel(nextScene);
+            //si el cliente es master, ambos jugadores cambian al mismo tiempo
+            //pero si no es master, hay que avisar al otro jugador (el master), que cambie de escena
+            if (PhotonNetwork.IsMasterClient) {
+                cambiarEscena();
+            } else {
+                view.RPC("cambiarEscena", RpcTarget.Others);
+            }
         }
+    }
+
+    [PunRPC]
+    void cambiarEscena() {
+        PhotonNetwork.LoadLevel(nextScene);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
