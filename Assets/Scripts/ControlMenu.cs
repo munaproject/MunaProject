bf63057516723452316b3f415cacef4b9ed981d6
Muna <muna.ongame@gmail.com>
2024-04-25 10:30:59 +0200

    menu provisional

---
+++ Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMenu : MonoBehaviour
+{
+    public void OnBotonJugar()
+    {
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    public void OnBotonCreditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void OnBotonSalir()
+    {
+        Application.Quit();
+    }
+
+    public void OnBotonMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnBotonInstrucciones()
+    {
+        SceneManager.LoadScene("Instrucciones");
+    }
+}
