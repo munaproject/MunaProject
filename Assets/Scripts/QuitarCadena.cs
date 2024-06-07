796b4b9412c62ef9567fe6ede8a251f02140787d
stacy <muna.ongame@gmail.com>
2024-06-07 12:18:24 +0200

    Cadena prefab

---
+++ Assets/Scripts/QuitarCadena.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class QuitarCadena : MonoBehaviour
+{
+
+    private bool activar;
+    private GameManager gameManager;
+    private PhotonView view;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activar = false;
+        gameManager = FindObjectOfType<GameManager>();
+        view = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && activar && gameManager.TieneLlave) {
+            view.RPC("autodestruir", RpcTarget.All);
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            activar = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        activar = false;
+    }
+
+    [PunRPC]
+    void autodestruir() {
+        Destroy(gameObject);
+    }
+}
