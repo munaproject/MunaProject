dda1c3e8bbca32b628f6ffc20de8ea8ec5f1b0fd
Muna <muna.ongame@gmail.com>
2024-06-07 12:11:59 +0200

    sonidos en los slimes
    caminos sigue sin funcionar, estoy harta

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,6 +9,7 @@ public class AudioManager : MonoBehaviour
 
     public AudioClip background;
     public AudioClip[] cambios;
+    public float[] volumes;
 
     public static AudioManager instance;
     private int indice;
@@ -16,9 +17,9 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
-            instance=this;
+            instance = this;
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -32,42 +33,53 @@ public class AudioManager : MonoBehaviour
         gameManager = FindObjectOfType<GameManager>();
         SceneManager.sceneLoaded += OnSceneLoaded;
         audioSource.clip = background;
+        audioSource.volume = volumes[0]; // Set initial volume
         audioSource.Play();
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (scene.name != "Menu" &&
-            scene.name != "Lobby"&&
+            scene.name != "Lobby" &&
             scene.name != "LoadingScene" &&
-            !gameManager.MusicaCargada && 
-            gameManager.CargarTodo) {
-
-                gameManager.MusicaCargada = true;
-                background=cambios[gameManager.IndiceMusica];
-                audioSource.clip = background;
-                audioSource.Play();
-            
-        } else {
-            if(scene.name == "MadreCinematica")
+            !gameManager.MusicaCargada &&
+            gameManager.CargarTodo)
+        {
+            gameManager.MusicaCargada = true;
+            indice = gameManager.IndiceMusica;
+            background = cambios[indice];
+            audioSource.clip = background;
+            audioSource.volume = volumes[indice]; // Set volume based on index
+            audioSource.Play();
+        }
+        else
+        {
+            if (scene.name == "MadreCinematica")
             {
-                indice=0;
-                background=cambios[indice];
+                indice = 0;
+                background = cambios[indice];
                 audioSource.clip = background;
+                audioSource.volume = volumes[indice]; // Set volume based on index
                 audioSource.Play();
             }
-            if(scene.name == "EntradaGanarEscena" || scene.name == "EntradaPerderEscena")
+            if (scene.name == "EntradaGanarEscena" || scene.name == "EntradaPerderEscena")
             {
-                indice=1;
-                background=cambios[indice];
+                indice = 1;
+                background = cambios[indice];
                 audioSource.clip = background;
+                audioSource.volume = volumes[indice]; // Set volume based on index
+                audioSource.Play();
+            }
+            if (scene.name == "CapituloDos")
+            {
+                indice = 2;
+                background = cambios[indice];
+                audioSource.clip = background;
+                audioSource.volume = volumes[indice]; // Set volume based on index
                 audioSource.Play();
             }
         }
-            
     }
 
-
-    //--
     public string Indice { get; set; }
 }
