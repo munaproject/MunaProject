09ede8b6f98be6fc73af8be1a11de89def847ba2
stacy <muna.ongame@gmail.com>
2024-06-09 14:10:11 +0200

    Pop up login

---
+++ Assets/Scripts/UIMensaje.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMensaje : MonoBehaviour
+{
+
+    public void cerrarMensaje() {
+        Destroy(gameObject);
+    }
+}
